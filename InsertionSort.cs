b7c71d1d83ffd7110f227f81e4169195ac24878e
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-22 18:32:06 +0530

    [Dhanya R Hegde] : Added Insertion Sort Problem

---
+++ InsertionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresProblems
+{
+    internal class InsertionSort
+    {
+        public void insertionSort(int[] arr) 
+        {
+            for(int i=1; i<arr.Length; i++)
+            {
+                int current = arr[i];
+                int j=i-1;
+
+                while(j>=0 && current < arr[j])
+                {
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+                arr[j+1] = current;
+            }
+        }
+
+        public void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,10 +7,15 @@
             //BubbleSort bubbleSort = new BubbleSort();
             //bubbleSort.BubbleSortArray();
 
-            int[] arr1 = { 4, 5, 3, 8, 1 };
-            MergeSort mergeSort = new MergeSort();
-            mergeSort.divide(arr1, 0, 4);
-            mergeSort.PrintArray(arr1);
+            //int[] arr1 = { 4, 5, 3, 8, 1 };
+            //MergeSort mergeSort = new MergeSort();
+            //mergeSort.divide(arr1, 0, 4);
+            //mergeSort.PrintArray(arr1);
+
+            int[] arr2 = { 4, 5, 3, 8, 1 };
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.insertionSort(arr2);
+            insertionSort.PrintArray(arr2 );
         }
     }
 }
